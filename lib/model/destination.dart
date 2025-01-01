0698563172d655beadbba907992ec684f83646e0
CristianS09 <redentorking90@gmail.com>
2025-01-01 19:22:53 -0300

    criando a classe de destino

---
+++ lib/model/destination.dart
@@ -0,0 +1,12 @@
+//Classe para representar os destinos
+class Destination {
+  final String title;
+  final String price;
+  final String url;
+  final double reviews;
+  final String description;
+
+  Destination({required this.title, required this.price, required this.url, required this.reviews, required this.description});
+
+  
+}
